1d0530b31e4a3c2cdbc7f564643dfa546eeb690f
cchchoi1986 <cchchoi1986@gmail.com>
2014-12-01 23:20:13 +0800

    fixed add items button format

--- js/application.js
+++ js/application.js
@@ -8,21 +8,12 @@ $(document).ready(function(){
   };
 
   var centsToDollars= function(cents) {
-    return "$" +(cents/100.00).toFixed(2);
-  };
-
-  var subtotal = [];
-
-  var totalPrice = function() {
-    var i = 0;
-    total = 0;
-    while (i < $('.item-price').length) {
-      // console.log($(itemPriceHTML[i]).text());
-      subtotal[i] = stripp($($('.item-price')[i]).text()) * Number($($('.quantity')[i]).val())
-      total += stripp($($('.item-price')[i]).text()) * Number($($('.quantity')[i]).val());
-      i++;
+    if (cents > 0) {
+      return "$" +(cents/100.00).toFixed(2);
     }
-    return total;
+    else {
+      return "$00.00";
+    }
   };
 
   var masterList = [
@@ -36,48 +27,62 @@ $(document).ready(function(){
     {name: 'Milk', price: 2011},
     {name: 'Sugar', price: 1001}
   ];
-  
-// updates subtotals and total in html
-  var refreshTotals = function(){
-    $('#total-price').text(centsToDollars(totalPrice()));
-    for (i in subtotal){
-      $($('.cc-item .total-cost')[i]).text(centsToDollars(subtotal[i]));
-    };
-  };
-
-  var hihi = function(){
-    var i=0;
-    var located = false;
-    $('.dropdown-menu li').removeClass('disabled');
-    while (i<$('.item-name').length){
-      j=0;
-      located = false;
-      while (j<$('.dropdown-menu li').length){
-        if ($.trim($($('.item-name')[i]).text())==$($('.dropdown-menu li a')[j]).text()){
-          $($('.dropdown-menu li')[j]).addClass('disabled');
-        }
-        j++;
-      }
-      i++;
-    }
-  };
-  hihi();
-
-// removes parent rows from button clicks
-  $(document).on('click','.cc-button',function() {
-    $(this).parent().remove();
-    refreshTotals();
-    hihi();
-  });
-
-  $('.quantity').blur(function(){
-    refreshTotals();
-    hihi();
-  });
 
   var itemRow =  function(item,price){
     return '<div id="'+item+'" class="row col-xs-12 cc-nomargin cc-item"><div class="item-name col-xs-2">'+item+'</div><div class="item-price col-xs-3">'+centsToDollars(price)+'</div><div class="item-qty col-xs-2"><input type="number" min="0" max="100" value="0" class="quantity col-xs-12"></div><div class="total-cost col-xs-3">$00.00</div><button class="cc-button col-xs-2">Remove</button></div>'
   };
+
+  var listRow = function(item){
+    return '<li role="presentation"><a class="cc-menu-list" role="menuitem" tabindex="-1" href="#">'+item+'</a></li>'
+  };
+
+  var totalPrice = function() {
+    var subtotal = [];
+    var i = 0;
+    total = 0;
+    $('.cc-item').removeClass('cc-item-even');
+    $('.quantity').removeClass('quantity-even');
+    // $('.dropdown-menu')[i].remove();
+    while (i < masterList.length) {
+      j=0;
+      while (j < $('.item-name').length){
+        if ($.trim($($('.item-name')[i]).text()) === masterList[j].name){
+          subtotal[i] = masterList[j].price * Number($($('.quantity')[i]).val());
+          total += masterList[j].price * Number($($('.quantity')[i]).val());
+          $($('.cc-item .item-price')[i]).text(centsToDollars(masterList[j].price));
+          $($('.cc-item .total-cost')[i]).text(centsToDollars(subtotal[i]));
+        }
+        j++;
+      }
+      if (i%2==0){
+        $($('.cc-item')[i]).addClass('cc-item-even');
+        $($('.quantity')[i]).addClass('quantity-even');
+      }
+      // $('.dropdown-menu').append(listRow(masterList[i].name));
+      i++;
+    }
+    $($('#total-price')).text(centsToDollars(total));
+  };
+
+// updates subtotals and total in html
+  // var hihi = function(){
+  //   var i=0;
+  //   var located = false;
+  //   $('.dropdown-menu li').removeClass('disabled');
+  //   while (i<$('.item-name').length){
+  //     j=0;
+  //     located = false;
+  //     while (j<$('.dropdown-menu li').length){
+  //       if ($.trim($($('.item-name')[i]).text())==$($('.dropdown-menu li a')[j]).text()){
+  //         $($('.dropdown-menu li')[j]).addClass('disabled');
+  //       }
+  //       j++;
+  //     }
+  //     i++;
+  //   }
+  // };
+  // hihi();
+  totalPrice();
 
   $('.dropdown-menu').on('click', 'a', function(){
     var i = 0;
@@ -86,10 +91,24 @@ $(document).ready(function(){
         $('.cc-header').after(itemRow($.trim($(this).text()),masterList[i].price));
       }
       i++;
+      // hihi();
+      totalPrice();
     }
   });
-  
 
+// removes parent rows from button clicks
+  $(document).on('click','.cc-button',function() {
+    $(this).parent().remove();
+    // hihi();
+    totalPrice();
+  });
+
+  $(document).on('blur','.quantity',function(){
+    // hihi();
+    totalPrice();
+  });
+
+  
 });
 
 
