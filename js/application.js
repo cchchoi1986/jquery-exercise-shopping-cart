b26fdbcdd161ab58b2e6a1f9ed9c8f025b462300
cchchoi1986 <cchchoi1986@gmail.com>
2014-12-01 13:42:13 +0800

    add items button

--- js/application.js
+++ js/application.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
   };
 
   var centsToDollars= function(cents) {
-    return "$" +(cents/100.00)+".00";
+    return "$" +(cents/100.00).toFixed(2);
   };
 
   var subtotal = [];
@@ -25,21 +25,72 @@ $(document).ready(function(){
     return total;
   };
 
-  var hihi = function(){
-    $('#total-price').text(centsToDollars(totalPrice()));
-    for (i in subtotal){
-      $($('.cc-item .total-cost')[i]).text(centsToDollars(subtotal[i]));
-    };
-  };
+  var masterList = [
+    {name: 'Salmon', price: 6899},
+    {name: 'Tuna', price: 5120},
+    {name: 'Carp', price: 4001},
+    {name: 'Pork', price: 5050},
+    {name: 'Beef', price: 4020},
+    {name: 'Chicken', price: 2511},
+    {name: 'Cereal', price: 4503},
+    {name: 'Milk', price: 2011},
+    {name: 'Sugar', price: 1001}
+  ];
 
-  $('.cc-button').click(function() {
+  var refreshTotals = function() {
+    var i = 0;
+    while (i < masterList.length){
+      $($('.cc-item .total-cost')[i]).text(centsToDollars($($('.quantity')[i]).val()*masterList[i].price));
+      $($('.item-price')[i]).text(centsToDollars(masterList[i].price));
+      $('#total-price').text(centsToDollars(totalPrice()));
+      i++;
+    }
+  }
+
+  var hihi = function(){
+    var i=0;
+    var located = false;
+    $('.dropdown-menu li').removeClass('disabled');
+    while (i<$('.item-name').length){
+      j=0;
+      located = false;
+      while (j<$('.dropdown-menu li').length){
+        if ($.trim($($('.item-name')[i]).text())==$($('.dropdown-menu li a')[j]).text()){
+          $($('.dropdown-menu li')[j]).addClass('disabled');
+        }
+        j++;
+      }
+      i++;
+    }
+  };
+  hihi();
+  refreshTotals();
+// removes parent rows from button clicks
+  $(document).on('click','.cc-button',function() {
     $(this).parent().remove();
+    refreshTotals();
     hihi();
   });
 
   $('.quantity').blur(function(){
+    refreshTotals();
     hihi();
   });
+
+  var itemRow =  function(item,price){
+    return '<div id="'+item+'" class="row col-xs-12 cc-nomargin cc-item"><div class="item-name col-xs-2">'+item+'</div><div class="item-price col-xs-3">'+centsToDollars(price)+'</div><div class="item-qty col-xs-2"><input type="number" min="0" max="100" value="0" class="quantity col-xs-12"></div><div class="total-cost col-xs-3">$00.00</div><button class="cc-button col-xs-2">Remove</button></div>'
+  };
+
+  $('.dropdown-menu').on('click', 'a', function(){
+    var i = 0;
+    while (i < masterList.length) {
+      if ($.trim($(this).text()) == masterList[i].name) {
+        $('.cc-header').after(itemRow($.trim($(this).text()),masterList[i].price));
+      }
+      i++;
+    }
+  });
+  
 
 });
 
